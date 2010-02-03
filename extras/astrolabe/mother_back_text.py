b9227426dd09e14e0d2944b2a1bd9999737ba8e8
dcf21 <dcf21@8e462c0f-ee70-4953-a96e-5db927b55778>
2010-02-03 14:31:13 +0000

    Work done on astrolabe and pyxplot logo
    git-svn-id: svn://127.0.0.1/pplsvn@600 8e462c0f-ee70-4953-a96e-5db927b55778

--- extras/astrolabe/mother_back_text.py
+++ extras/astrolabe/mother_back_text.py
@@ -1,7 +1,8 @@
 # Horrible fudge way to write text in circles around the mother
 # $Id$
 
-out = open("mother_back_text.dat","w")
+out1 = open("mother_back_text.dat","w")
+out2 = open("mother_back_text_simple.dat","w")
 
 # Signs of the zodiac
 strings = [["ARIES",r"\aries"],["TAURUS",r"\taurus"],["GEMINI",r"\gemini"],["CANCER",r"\cancer"],["LEO",r"\leo"],["VIRGO",r"\virgo"],["LIBRA",r"\libra"],["SCORPIO",r"\scorpio"],["SAGITTARIUS",r"\sagittarius"],["CAPRICORNUS",r"\capricornus"],["AQUARIUS",r"\aquarius"],["PISCES",r"\pisces"]]
@@ -14,9 +15,11 @@ for i in range(len(strings)):
  ang = 15 + 30*i
 
  for j in range(len(word)):
-  out.write("RT = RT1 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang - LetterSep*L/2 + j*LetterSep, word[j]))
+  out1.write("RT = RT1 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang - LetterSep*L/2 + j*LetterSep, word[j]))
+  out2.write("RT = RT1 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang - LetterSep*L/2 + j*LetterSep, word[j]))
  j=j+2
- out.write("RT = RT1 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang - LetterSep*L/2 + j*LetterSep, after))
+ out1.write("RT = RT1 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang - LetterSep*L/2 + j*LetterSep, after))
+ out2.write("RT = RT1 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang - LetterSep*L/2 + j*LetterSep, after))
 
 # Months
 strings = [["JANUARY",-79],["FEBRUARY",-48],["MARCH",-20],["APRIL",11],["MAY",41],["JUNE",72-1],["JULY",102-2],["AUGUST",133-2],["SEPTEMBER",164-4],["OCTOBER",194-3],["NOVEMBER",225-2],["DECEMBER",255-2]]
@@ -31,7 +34,9 @@ for i in range(len(strings)):
  ang2 = (daynum+15+9)*360.0/365.0
 
  for j in range(len(word)):
-  out.write("RT = RT2 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang1 - LetterSep1*L/2 + j*LetterSep1, word[j]))
-  out.write("RT = RT3 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang2 - LetterSep2*L/2 + j*LetterSep2, word[j]))
+  out1.write("RT = RT2 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang1 - LetterSep1*L/2 + j*LetterSep1, word[j]))
+  out2.write("RT = RT2 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang1 - LetterSep1*L/2 + j*LetterSep1, word[j]))
+  out1.write("RT = RT3 ; theta=%f*unit(deg) ; text \"%s\" at RT*cos(theta),RT*sin(theta) rot theta+90*unit(deg) hal c val c\n"%(ang2 - LetterSep2*L/2 + j*LetterSep2, word[j]))
 
-out.close()
+out1.close()
+out2.close()
