9814c0b64fccc61e7b9aceec6e940e0c4fa038e1
rpc25 <rpc25@8e462c0f-ee70-4953-a96e-5db927b55778>
2010-05-18 11:03:12 +0000

    Antique documentation
    git-svn-id: svn://127.0.0.1/pplsvn@797 8e462c0f-ee70-4953-a96e-5db927b55778

---
+++ extras/website/source/HTMLdoc/0.5/labels.pl
@@ -0,0 +1,541 @@
+# LaTeX2HTML 2002-2-1 (1.70)
+# Associate labels original text with physical files.
+
+
+$key = q/v_zrelate/;
+$external_labels{$key} = "$URL/" . q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/powerpoint_example/;
+$external_labels{$key} = "$URL/" . q|node52.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_colours/;
+$external_labels{$key} = "$URL/" . q|node30.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/horizontal_datafiles/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ranges_multiaxes/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/correlation_matrix/;
+$external_labels{$key} = "$URL/" . q|node66.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/configuration/;
+$external_labels{$key} = "$URL/" . q|node42.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/examples/;
+$external_labels{$key} = "$URL/" . q|node48.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex8/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex5/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/first_plots/;
+$external_labels{$key} = "$URL/" . q|node10.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sample_datafile/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/missing_features/;
+$external_labels{$key} = "$URL/" . q|node17.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/plot_datafiles/;
+$external_labels{$key} = "$URL/" . q|node11.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/multiple_axes/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/gnuplot_intro/;
+$external_labels{$key} = "$URL/" . q|node8.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/axis_removal/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/introduction/;
+$external_labels{$key} = "$URL/" . q|node2.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/directing_output/;
+$external_labels{$key} = "$URL/" . q|node12.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/configfile_terminal/;
+$external_labels{$key} = "$URL/" . q|node46.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/barcharts/;
+$external_labels{$key} = "$URL/" . q|node33.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/colour_names/;
+$external_labels{$key} = "$URL/" . q|node47.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_arrow/;
+$external_labels{$key} = "$URL/" . q|node28.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_display/;
+$external_labels{$key} = "$URL/" . q|node32.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/spline_command/;
+$external_labels{$key} = "$URL/" . q|node38.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/arrows_plot_style/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fit_math/;
+$external_labels{$key} = "$URL/" . q|node61.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/linked_axes/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/config_files/;
+$external_labels{$key} = "$URL/" . q|node43.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/plot_ranges/;
+$external_labels{$key} = "$URL/" . q|node14.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_terminal2/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/L_taylor_expand/;
+$external_labels{$key} = "$URL/" . q|node64.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/gallery/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex3/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Q_vector/;
+$external_labels{$key} = "$URL/" . q|node64.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/arrows/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/text_command/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex11/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/errorbars/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/select_modifier/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex10/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fit_command/;
+$external_labels{$key} = "$URL/" . q|node15.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bayes_pdf/;
+$external_labels{$key} = "$URL/" . q|node63.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex7/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/new_features/;
+$external_labels{$key} = "$URL/" . q|node18.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/example7/;
+$external_labels{$key} = "$URL/" . q|node55.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex9/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pointtype_modifier/;
+$external_labels{$key} = "$URL/" . q|node13.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/axis_extensions/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ex11/;
+$external_labels{$key} = "$URL/" . q|node59.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ex10/;
+$external_labels{$key} = "$URL/" . q|node58.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex6/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/filebackup/;
+$external_labels{$key} = "$URL/" . q|node26.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bayes_notation/;
+$external_labels{$key} = "$URL/" . q|node62.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/finding_sigmai/;
+$external_labels{$key} = "$URL/" . q|node67.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex1/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/p_f_given_sigma/;
+$external_labels{$key} = "$URL/" . q|node67.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/nolabelstics/;
+$external_labels{$key} = "$URL/" . q|node22.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/configfile_settings/;
+$external_labels{$key} = "$URL/" . q|node45.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/def_covar/;
+$external_labels{$key} = "$URL/" . q|node65.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex2/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/multiplot/;
+$external_labels{$key} = "$URL/" . q|node31.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/splice/;
+$external_labels{$key} = "$URL/" . q|node37.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminals/;
+$external_labels{$key} = "$URL/" . q|node20.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex4/;
+$external_labels{$key} = "$URL/" . q|node60.html|; 
+$noresave{$key} = "$nosave";
+
+$key = q/example6/;
+$external_labels{$key} = "$URL/" . q|node54.html|; 
+$noresave{$key} = "$nosave";
+
+1;
+
+
+# LaTeX2HTML 2002-2-1 (1.70)
+# labels from external_latex_labels array.
+
+
+$key = q/v_zrelate/;
+$external_latex_labels{$key} = q|6.12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/powerpoint_example/;
+$external_latex_labels{$key} = q|5.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_colours/;
+$external_latex_labels{$key} = q|3.4.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/horizontal_datafiles/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ranges_multiaxes/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/correlation_matrix/;
+$external_latex_labels{$key} = q|6.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/configuration/;
+$external_latex_labels{$key} = q|4.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/examples/;
+$external_latex_labels{$key} = q|5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex8/;
+$external_latex_labels{$key} = q|5.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex5/;
+$external_latex_labels{$key} = q|5.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/first_plots/;
+$external_latex_labels{$key} = q|2.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/sample_datafile/;
+$external_latex_labels{$key} = q|2.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/missing_features/;
+$external_latex_labels{$key} = q|2.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/plot_datafiles/;
+$external_latex_labels{$key} = q|2.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/multiple_axes/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/gnuplot_intro/;
+$external_latex_labels{$key} = q|2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/axis_removal/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/introduction/;
+$external_latex_labels{$key} = q|1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/directing_output/;
+$external_latex_labels{$key} = q|2.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/configfile_terminal/;
+$external_latex_labels{$key} = q|4.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/barcharts/;
+$external_latex_labels{$key} = q|3.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/colour_names/;
+$external_latex_labels{$key} = q|4.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_arrow/;
+$external_latex_labels{$key} = q|3.4.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_display/;
+$external_latex_labels{$key} = q|3.5.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/spline_command/;
+$external_latex_labels{$key} = q|3.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/arrows_plot_style/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fit_math/;
+$external_latex_labels{$key} = q|6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/linked_axes/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/config_files/;
+$external_latex_labels{$key} = q|4.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/plot_ranges/;
+$external_latex_labels{$key} = q|2.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/set_terminal2/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/L_taylor_expand/;
+$external_latex_labels{$key} = q|6.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/gallery/;
+$external_latex_labels{$key} = q|5.12|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex3/;
+$external_latex_labels{$key} = q|5.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/Q_vector/;
+$external_latex_labels{$key} = q|6.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/arrows/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/text_command/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex11/;
+$external_latex_labels{$key} = q|5.11|; 
+$noresave{$key} = "$nosave";
+
+$key = q/errorbars/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/select_modifier/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex10/;
+$external_latex_labels{$key} = q|5.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fit_command/;
+$external_latex_labels{$key} = q|2.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bayes_pdf/;
+$external_latex_labels{$key} = q|6.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex7/;
+$external_latex_labels{$key} = q|5.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/new_features/;
+$external_latex_labels{$key} = q|3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/example7/;
+$external_latex_labels{$key} = q|5.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex9/;
+$external_latex_labels{$key} = q|5.9|; 
+$noresave{$key} = "$nosave";
+
+$key = q/pointtype_modifier/;
+$external_latex_labels{$key} = q|2.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/axis_extensions/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ex11/;
+$external_latex_labels{$key} = q|5.11|; 
+$noresave{$key} = "$nosave";
+
+$key = q/ex10/;
+$external_latex_labels{$key} = q|5.10|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex6/;
+$external_latex_labels{$key} = q|5.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/filebackup/;
+$external_latex_labels{$key} = q|3.3|; 
+$noresave{$key} = "$nosave";
+
+$key = q/bayes_notation/;
+$external_latex_labels{$key} = q|6.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/finding_sigmai/;
+$external_latex_labels{$key} = q|6.6|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex1/;
+$external_latex_labels{$key} = q|5.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/p_f_given_sigma/;
+$external_latex_labels{$key} = q|6.18|; 
+$noresave{$key} = "$nosave";
+
+$key = q/nolabelstics/;
+$external_latex_labels{$key} = q|3.3.1|; 
+$noresave{$key} = "$nosave";
+
+$key = q/configfile_settings/;
+$external_latex_labels{$key} = q|4.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/def_covar/;
+$external_latex_labels{$key} = q|6.8|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex2/;
+$external_latex_labels{$key} = q|5.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/multiplot/;
+$external_latex_labels{$key} = q|3.5|; 
+$noresave{$key} = "$nosave";
+
+$key = q/splice/;
+$external_latex_labels{$key} = q|3.7|; 
+$noresave{$key} = "$nosave";
+
+$key = q/terminals/;
+$external_latex_labels{$key} = q|3.2|; 
+$noresave{$key} = "$nosave";
+
+$key = q/fig_ex4/;
+$external_latex_labels{$key} = q|5.4|; 
+$noresave{$key} = "$nosave";
+
+$key = q/example6/;
+$external_latex_labels{$key} = q|5.6|; 
+$noresave{$key} = "$nosave";
+
+1;
+
